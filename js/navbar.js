a6b62f1a7279c8c6624a0569f12dc98d01d7bf2e
Jaskiran <jaskiranchhokar@gmail.com>
2020-04-23 01:53:12 -0400

    Refactor navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -2,18 +2,18 @@
 const navbarToggle = () => {
     const navbarIcon = document.querySelector('.navbar__icon');
     const navbarContain = document.querySelector('.navbar__mobile--contain');
-    
     document.querySelector('.navbar__mobile--button').addEventListener('click', e => {
         navbarIcon.classList.toggle('fa-times');
         navbarContain.classList.toggle('display--none');
     }); 
 }
 
-navbarToggle();
-
+//Change navbar style on scroll
 const navbar = document.querySelector('.navbar__container');
 window.addEventListener('scroll', e => {
     window.scrollY === 0 ? navbar.classList.remove('background--black') : navbar.classList.add('background--black'); 
 }); 
+
+navbarToggle();
 
 
